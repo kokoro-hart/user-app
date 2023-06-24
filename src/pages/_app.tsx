bc11d35071d0be0e764fa3b0bfd700d10746aa89
kokoro-hart <tobita@wd-flat.com>
2023-06-24 15:15:50 +0900

    feat: chakra-ui

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,16 @@
+import { ChakraProvider } from "@chakra-ui/react";
 import type { AppProps } from "next/app";
+
 import "ress";
 
 import "@/styles/globals.css";
 
 const App = ({ Component, pageProps }: AppProps) => {
-  return <Component {...pageProps} />;
+  return (
+    <ChakraProvider>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 };
 
 export default App;
